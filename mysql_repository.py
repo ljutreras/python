7d242a70d7e7e19a471768b2bf78ddf9c6a79b3b
ljutreras <ljutreras96@gmail.com>
2023-11-23 23:57:07 -0300

    fix: u_id

--- mysql_repository.py
+++ mysql_repository.py
@@ -1,6 +1,6 @@
 def insert_query(table):
     #insertar los datos
     if table == 'assistant':
-        return f"INSERT INTO {table}(uid, role, content, date) VALUES (%s, %s, %s, DATE_ADD(NOW(), INTERVAL 1 MINUTE))"
+        return f"INSERT INTO {table}(uid, role, content, date, u_id) VALUES (%s, %s, %s, DATE_ADD(NOW(), INTERVAL 1 MINUTE), LAST_INSERT_ID())"
     elif table == 'users':
         return f"INSERT INTO {table}(uid, role, content, date) VALUES (%s, %s, %s, NOW())"
