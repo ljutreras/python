9bc9a30d0b5c5e4e71bdcb2af0ea0b9bfec3a847
ljutreras <ljutreras96@gmail.com>
2023-11-28 23:34:35 -0300

    fead: prompt detalle and generate a new prompt to recibo and formas de pago

--- chat/main.py
+++ chat/main.py
@@ -4,7 +4,6 @@ import os
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from db.mysql_repository import connect_to_database
-from db.insert_db import insert_db
 from servicios.detalle import detalle_de_la_deuda
 from servicios.recibo import solicitar_recibo
 from servicios.formas_y_lugares import formas_y_lugares_de_pago
@@ -20,28 +19,13 @@ class User(BaseModel):
 
 openai.api_key  = os.getenv('API_KEY')
 
-def chat_movistar():
-    try:
-        context = [ {'role':'system', 'content':"""
-                            Bienveni@ al chat de Movistar, un servicio que entregar la deuda del servicio telefonico de un usuario. \
-                            Primero saludas al cliente \
-                            luego le dices tus servicios y en que puedes ayudarlo, \
-                            luego pregunta si necesita ayuda con los conocer detalle de la deuda, formas y lugares de pago o solicitar recibo. \
-                            cuando el usuario responda le mencionas que necesitas consultar algunos datos para continuar con tu consulta. Por favor, ingresa el documento de identidad del titular del servicio\
-                            Espera para tener la solicitud completa y comprueba si hay un final \
-                            dale tiempo para saber si el cliente quiere añadir algo más. \
-                            Asegúrese de aclarar todas las opciones \
-                            identificar el elemento del menú.\
-                            Respondes en un estilo amigable breve y muy conversacional. \
-                            Para finalizar la conversación debes mencionarle que ingrese la palabra SALIR para terminar la conversacion. \
-                            """
-                        } ]
-        tools = [
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    tools = [
                 {
                     "type": "function",
                     "function": {
                         "name": "detalle_de_la_deuda",
-                        "description": "obtiene el detalle de la deuda desde la base de datos",
+                        "description": "obtiene el detalle de la deuda desde la base de datos mediante un prompt",
                         "parameters": {
                             "type": "object",
                             "properties": {}
@@ -70,51 +54,45 @@ def chat_movistar():
                         }
                     },
                 }
-
             ]
-        response = openai.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            messages=context,
-            tools=tools,
-            tool_choice="auto",
-            temperature=0
-        )
-            
-        response_content = response.choices[0].message.content if response.choices else ""
-        response_message = response.choices[0].message
-        tool_calls = response_message.tool_calls
-        if tool_calls:
-            available_functions = {
-                "detalle_de_la_deuda": detalle_de_la_deuda,
-                "solicitar_recibo": solicitar_recibo,
-                "formas_y_lugares_de_pago": formas_y_lugares_de_pago,
-                }
-            context.append(response_message)
-            for tool_call in tool_calls:
-                function_name = tool_call.function.name
-                function_to_call = available_functions[function_name]
-                function_response = function_to_call()
-                context.append(
-                    {
-                        "tool_call_id": tool_call.id,
-                        "role": "tool",
-                        "name": function_name,
-                        "content": function_response,
-                    }
-                )
-            second_response = openai.chat.completions.create(
-                model="gpt-3.5-turbo-1106",
-                context=context,
-                temperature=0
-            )
-            print(context)
-            print(f'Assistant: {second_response.choices[0].message.content}')
-        else:
-            print(context)
-            print(f'Assistant: {response_content}')
+    response = openai.chat.completions.create(
+        model=model,
+        messages=messages,
+        tools=tools,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    
+    response_message = response.choices[0].message
+    tool_calls = response_message.tool_calls
 
-    except:
-        pass
+    if tool_calls:
+        available_functions = {
+            "detalle_de_la_deuda": detalle_de_la_deuda,
+            "solicitar_recibo": solicitar_recibo,
+            "formas_y_lugares_de_pago": formas_y_lugares_de_pago
+            }
+        messages.append(response_message)
+        
+        for tool_call in tool_calls:
+            function_name = tool_call.function.name
+            function_to_call = available_functions[function_name]
+            function_response = function_to_call()
+            messages.append(
+                {
+                    "tool_call_id": tool_call.id,
+                    "role": "tool",
+                    "name": function_name,
+                    "content": function_response,
+                }
+            )
+
+        second_response = openai.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=messages,
+        )
+        return second_response.choices[0].message.content
+    else:
+        return response_message.content
 
 
 context = [ {'role':'system', 'content':"""
@@ -131,16 +109,13 @@ Para finalizar la conversación debes mencionarle que ingrese la palabra SALIR p
 """
 } ]
 
-
-
-def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
-    response = openai.chat.completions.create(
-        model=model,
-        messages=messages,
-        temperature=temperature, # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message.content
-
+# def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    # response = openai.chat.completions.create(
+    #     model=model,
+    #     messages=messages,
+    #     tools=tools,
+    #     temperature=temperature, # this is the degree of randomness of the model's output
+    # )
 
 while True:
     input_user = input('User: ')
@@ -151,5 +126,3 @@ while True:
         print(f'Assistant: {response}')
     else:
         break
-
-
--- chat/servicios/recibo.py
+++ chat/servicios/recibo.py
@@ -1,2 +1,28 @@
 def solicitar_recibo():
-    return 'solicitar recibo'
+    prompt = f"""Tu tarea es dar el detalle de la deuda de un usuario
+            creando una descripcion que contenga
+            DETALLE DE LA DEUDA
+            <NOMBRE> Tienes un recibo pendiente de tu servicio
+            <MOVIL/HOGAR>
+            <NRO_SERVICIO> que venció el
+            <DD/MM> por CLP <MONTO_DEUDA>
+
+            los datos dentro de las <> para los campos
+            NOMBRE
+            NRO_SERVICIO
+            DD/MM
+            MONTO DEUDA
+            deben ser aleatorios
+            la informacion obtenida será delimitada por
+            triples comillas invertidas.
+
+            datos del usuario: ```{message}```
+            """
+    return prompt
+
+""" 
+SOLICITAR RECIBO
+Obten tu recibo con solo unos clics
+https://mirecibo.movistar.com.pe
+
+"""
