09b113e466e616c0b672e877c8421f312f33a059
IgnacioAguileraR <ignacioaguilerarodriguez7@gmail.com>
2023-12-04 15:12:51 -0300

    add assistants.py

---
+++ assistants.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI()
+""" 
+curl "https://api.openai.com/v1/assistants" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -H "OpenAI-Beta: assistants=v1" \
+  -d '{
+    "instructions": "You are a personal math tutor. When asked a question, write and run Python code to answer the question.",
+    "name": "Math Tutor",
+    "tools": [{"type": "code_interpreter"}],
+    "model": "gpt-4"
+  }' """
