ff647cd309144594abcfccc1a6346b060f2d5fd0
IgnacioAguileraR <ignacioaguilerarodriguez7@gmail.com>
2023-12-06 17:56:04 -0300

    feat: add prueba_string

---
+++ prueba_string.py
@@ -0,0 +1,9 @@
+def strings():
+
+    string_list = "1,2,3,4,5,6,7,8,9"
+
+    return string_list
+
+string_list = strings()
+
+print(f"esta es una lista de strings: {string_list}. y no tienes que hacer nada mas")
